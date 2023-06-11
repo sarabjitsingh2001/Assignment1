cc0b688ef67a134d70746b4c4f044f2291602acb
sarabjitsingh2001 <131690605+sarabjitsingh2001@users.noreply.github.com>
2023-06-11 22:49:10 +0530

    Create S12.java

---
+++ S12.java
@@ -0,0 +1,41 @@
+public class S12 {
+
+    public static void main(String[] args) {
+
+        String inputString = "abbbacdbbbdddde";
+
+        String outputString = outputString(inputString, 0); // Fixed
+
+        System.out.println("Output String is: " + outputString);
+    }
+
+    static String outputString(String inputString, int i) {
+
+        if (inputString == null)
+            return null;
+
+        if ((i > inputString.length() - 2)) {
+
+            if (inputString.length() < 2)
+                return inputString;
+            else if (inputString.charAt(i - 1) == inputString.charAt(i))
+                return "";
+            else
+                return inputString.charAt(i) + "";
+
+        }
+
+        char temp1 = inputString.charAt(i);
+        String temp2 = outputString(inputString, i + 1);
+
+        if (temp1 == inputString.charAt(i + 1)) {
+            if (temp2.length() > 1 && temp1 == temp2.charAt(0))
+                return temp1 + "#" + temp2.substring(1);
+            else
+                return temp1 + "#" + temp2;
+        }
+
+        return temp1 + outputString(inputString, i + 1);
+    }
+
+}
